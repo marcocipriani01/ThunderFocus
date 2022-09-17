9288e3e2f8b989cd9f07aac6ac3f5ee1a1853ba1
marcocipriani01 <marco.cipriani.01@gmail.com>
2022-09-17 16:00:04 +0200

    Update config

--- ThunderFocus-firmware/src/boards/thunderfocus_pcb_v2_flat.h
+++ ThunderFocus-firmware/src/boards/thunderfocus_pcb_v2_flat.h
@@ -9,7 +9,7 @@
 // ===================================================================================
 #define ENABLE_DEVMAN true
 #if ENABLE_DEVMAN == true
-#define MANAGED_PINS_COUNT 9
+#define MANAGED_PINS_COUNT 8
 #define MANAGED_PINS {{ 1,  0,  false,   false,  false}, \
                     {   2,  0,  false,   false,  false}, \
                     {   3,  0,  true,    false,  false}, \
@@ -17,7 +17,6 @@
                     {   5,  0,  false,   false,  false}, \
                     {   6,  0,  true,    false,  false}, \
                     {   7,  0,  false,   false,  false}, \
-                    {   8,  0,  false,   false,  false}, \
                     {   9,  0,  false,   false,  false}}
 #define STATUS_LED_MANAGED true
 
@@ -26,4 +25,19 @@
 
 // ---------- Time ----------
 #define RTC_SUPPORT SERIAL_TIME
+#endif
+
+// ===================================================================================
+// ================================ FLAT PANEL CONFIG ================================
+// ===================================================================================
+#define FLAT_PANEL true
+#if FLAT_PANEL == true
+#define EL_PANEL_PIN 10
+#define EL_PANEL_LOG_SCALE true
+#define EL_PANEL_ON_BOOT false
+#define SERVO_MOTOR SERVO_RDS3225
+#if defined(SERVO_MOTOR) && SERVO_MOTOR != OFF
+#define SERVO_PIN 17
+#endif
+
 #endif
